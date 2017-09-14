5604c1934501df4c73609adb855303bbb74ea4e9
daniellzl <mr.daniel.liang@gmail.com>
2017-09-13 20:58:28 -0700

    added comments about masonry

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
+// load  masonry.js grid
 var grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
   columnWidth: '.grid-sizer'
 });
 
+// when images are loaded arrange according to masonry grid
 grid.imagesLoaded().progress(function() {
   grid.masonry();
 });

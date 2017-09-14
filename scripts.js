379f8e206bbfc12bde202be4541b316cc0f13680
daniellzl <mr.daniel.liang@gmail.com>
2017-09-13 20:56:31 -0700

    revised project

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 var grid = $('.grid').masonry({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer'
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
 });
 
 grid.imagesLoaded().progress(function() {
-    grid.masonry();
+  grid.masonry();
 });
